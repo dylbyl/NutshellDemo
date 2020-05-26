06f8e5a3dbded41dd2fa4180c1cd79be8e8ca41a
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-05-26 11:53:25 -0400

    Fixed chat for login. Put on enter key press for register, Styled buttons, hid logout button when user was logged out. Added refresh to fix user logout/in chat

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,9 +1,10 @@
-import registerDomPrinter from './registerDomPrinter.js'
-import registerEventListener from './registerEventListener.js'
+import registerDomPrinter from './registerDomPrinter.js';
+import registerEventListener from './registerEventListener.js';
+import chatEventListeners from './chatEventListeners.js';
 
 //The HTML for the email and password in the modal
 const loginForm = () => {
-    return `
+	return `
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" id="login-email" name="email" required>
 
@@ -12,75 +13,74 @@ const loginForm = () => {
 
     <button id="login-btn" class="login-btn">Login</button>
 
-    `
-}
-
+    `;
+};
 
 const nutLogin = {
-    //Launches the login modal by changing the display to block
-    activateModal() {
-        document.querySelector('#login-modal').style.display = 'block';
-    },
-    //listens for the login button and attempts to log in with that email and password
-    loginEventListenter() {
-        document.querySelector('#login-btn').addEventListener("click", () => {
-            const emailValue = document.querySelector("#login-email").value;
-            const passwordValue = document.querySelector("#login-password").value;
+	//Launches the login modal by changing the display to block
+	activateModal() {
+		document.getElementById('logout').style.display = 'none';
+		document.querySelector('#login-modal').style.display = 'block';
+	},
+	//listens for the login button and attempts to log in with that email and password
+	loginEventListenter() {
+		document.querySelector('#login-btn').addEventListener('click', () => {
+			const emailValue = document.querySelector('#login-email').value;
+			const passwordValue = document.querySelector('#login-password').value;
 
-            //fetches any user with the an email and password matching the inputs
-            fetch(`http://localhost:8088/users?email=${emailValue}&password=${passwordValue}`)
-                .then((r) => r.json())
-                .then((user) => {
-                    //If a user is returned
-                    if (user.length != 0) {
-                        //Sets the userId in session storage to the id of the user that was fetched, deactivates the modal, and clears the values in the form
-                        sessionStorage.setItem("userId", user[0].id);
-                        nutLogin.deactivateModal();
-                        document.querySelector("#login-email").value = "";
-                        document.querySelector("#login-password").value = "";
+			//fetches any user with the an email and password matching the inputs
+			fetch(`http://localhost:8088/users?email=${emailValue}&password=${passwordValue}`)
+				.then((r) => r.json())
+				.then((user) => {
+					//If a user is returned
+					if (user.length != 0) {
+						//Sets the userId in session storage to the id of the user that was fetched, deactivates the modal, and clears the values in the form
+						sessionStorage.setItem('userId', user[0].id);
+						nutLogin.deactivateModal();
+						// Forcing reload of page to refresh and clear the user correctly
+						location.reload(true);
+						document.querySelector('#login-email').value = '';
+						document.querySelector('#login-password').value = '';
 
-                        //If it didn't find a match it returns an error message
-                    } else {
-                        document.querySelector("#login-error-container").innerHTML = "email or password is incorrect!"
-                        
-                    }
-                }
-                )
+						//If it didn't find a match it returns an error message
+					} else {
+						document.querySelector('#login-error-container').innerHTML = 'email or password is incorrect!';
+					}
+				});
+		});
+	},
+	//Deactiaves the modal by setting the display to none
+	deactivateModal() {
+		document.querySelector('#login-modal').style.display = 'none';
+	},
 
-        })
-    },
-    //Deactiaves the modal by setting the display to none
-    deactivateModal() {
-        document.querySelector('#login-modal').style.display = 'none';
-    },
+	registerLinkListener() {
+		document.querySelector('#register-link').addEventListener('click', () => {
+			// Directing the user to the register form page when the site loads
+			registerDomPrinter.printRegisterForm();
+			document.querySelector(`#register-user-submit`).addEventListener('click', (clickEvent) => {
+				registerEventListener.submitRegisterForm();
+			});
+		});
+	},
 
-    registerLinkListener() {
-        document.querySelector('#register-link').addEventListener("click", () => {
-            
-            // Directing the user to the register form page when the site loads
-            registerDomPrinter.printRegisterForm()
-            document.querySelector(`#register-user-submit`).addEventListener("click", clickEvent => {
-                registerEventListener.submitRegisterForm();
-        
-            })
-        })
-    },
+	//Prints the login form in the modal.  This method is used after an account is successfully registered.
+	loginFormPrinter() {
+		document.getElementById('logout').style.display = 'none';
+		document.querySelector(`#login-container`).innerHTML = loginForm();
+	},
 
-    //Prints the login form in the modal.  This method is used after an account is successfully registered.
-    loginFormPrinter() {
-        document.querySelector(`#login-container`).innerHTML = loginForm()
-    },
+	//Listens for the logout button, sets the userId in session storage to null, and activates the login modal.
+	logoutListener() {
+		document.querySelector('#logout').addEventListener('click', () => {
+			sessionStorage.setItem('userId', null);
+			sessionStorage.clear();
+			// Redirecting to splash page
+			window.location.href = 'http://127.0.0.1:5500/';
+			// Forcing reload of page
+			location.reload(true);
+		});
+	}
+};
 
-    //Listens for the logout button, sets the userId in session storage to null, and activates the login modal.
-    logoutListener() {
-        document.querySelector("#logout").addEventListener("click", () => {
-            sessionStorage.setItem("userId", null);
-            nutLogin.activateModal();
-        })
-    }
-
-}
-
-
-
-export default nutLogin
+export default nutLogin;
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,31 +1,28 @@
-import event_eventListener from "./event-eventListeners.js"
-import nutLogin from './login.js'
-
-
+import event_eventListener from './event-eventListeners.js';
+import nutLogin from './login.js';
+import registerEventListener from './registerEventListener.js';
 
 // sessionStorage.setItem("userId", 1)
 // sessionStorage.setItem("username", "user1")
 
-if (sessionStorage.getItem("userId") == null){
-	nutLogin.activateModal()
-} 
+if (sessionStorage.getItem('userId') == null) {
+	nutLogin.activateModal();
+}
 
 nutLogin.loginEventListenter();
 nutLogin.registerLinkListener();
 nutLogin.logoutListener();
 //Calling all event listeners for 'Events'
-event_eventListener.eventsPageListener()
-event_eventListener.addNewEventListener()
-event_eventListener.deleteEventListener()
-event_eventListener.editEventListener()
-event_eventListener.saveEventListener()
+event_eventListener.eventsPageListener();
+event_eventListener.addNewEventListener();
+event_eventListener.deleteEventListener();
+event_eventListener.editEventListener();
+event_eventListener.saveEventListener();
 
 // Importing the chat event listener functions
 import chatEventListeners from './chatEventListeners.js';
 // Importing the chat api functions
 import chatAPI from './chatAPI.js';
-
-
 
 // Creating an event listener for when the user clicks on the chat tab on the nav bar
 document.querySelector(`#chat-page`).addEventListener('click', (chatEvent) => {
@@ -56,26 +53,35 @@ document.getElementById('output-container').onkeydown = function(e) {
 		}
 	}
 };
-
+// Creating event listeners for enter key press on the register and login screens
+document.getElementById(`login-container`).onkeydown = function(e) {
+	if (e.keyCode == 13) {
+		if (event.target.id.includes(`register-password`)) {
+			registerEventListener.submitRegisterForm();
+		} else if (event.target.id.includes(`login-password`)) {
+			event.preventDefault();
+			document.getElementById('login-btn').click();
+		}
+	}
+};
 
 //Imports functions for DOM printing and EventListeners
-import newsPrinterFunctions from "./newsPrinter.js";
-import newsListenFunctions from "./newsListeners.js";
-import tasksEvents from "./tasksEventListener.js";
+import newsPrinterFunctions from './newsPrinter.js';
+import newsListenFunctions from './newsListeners.js';
+import tasksEvents from './tasksEventListener.js';
 
 //If the News link in the Nav Bar is clicked, call a function to print the News section
-document.querySelector("#news-page").addEventListener("click", function() {
-    newsPrinterFunctions.printInitialPage();
-})
+document.querySelector('#news-page').addEventListener('click', function() {
+	newsPrinterFunctions.printInitialPage();
+});
 
 //Checks which buttons are being clicked in the News section
-document.querySelector("#output-container").addEventListener("click", function() {
-        newsListenFunctions.checkButton();
-    })
+document.querySelector('#output-container').addEventListener('click', function() {
+	newsListenFunctions.checkButton();
+});
 
 tasksEvents.tasksEventListener();
 
 tasksEvents.tasksPageEventListeners();
 
 tasksEvents.taskEditKeypressListener();
-   
